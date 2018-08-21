befb192f24ac7c3b4d59fa3358deb9cd9ce149e9
ElliMaija <elli.salo@outlook.com>
2018-08-21 14:33:21 +0300

    Uusia actionresultteja Apissa

--- kimppakyyti-api/KimppakyytiApi/KimppakyytiApi/Controllers/UserController.cs
+++ kimppakyyti-api/KimppakyytiApi/KimppakyytiApi/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Configuration;
+using KimppakyytiApi.Models;
 
 namespace KimppakyytiApi.Controllers
 {
@@ -43,13 +44,42 @@ namespace KimppakyytiApi.Controllers
             return "Nyt on tehty collection, vaikka sitä ei oltu tehty aiemmin!";
         }
         [HttpPost]
-        public async Task<ActionResult<string>> Post([FromBody] KimppakyytiApi.Models.User value)
+        public async Task<ActionResult<string>> Post([FromBody] Models.User value)
         {
             Document document = await _client.CreateDocumentAsync(
           UriFactory.CreateDocumentCollectionUri(_dbName, _collectionName),
           value);
             return Ok(document.Id);
         }
-        
+        [HttpGet]
+        public ActionResult<List<Models.User>> GetAllUsers()
+        {
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 };
+            IQueryable<Models.User> query = _client.CreateDocumentQuery<Models.User>(
+            UriFactory.CreateDocumentCollectionUri(_dbName, _collectionName),
+            $"SELECT * FROM C",
+            queryOptions);
+            return Ok(query.ToList());
+        }
+        [HttpGet]
+        public async Task<ActionResult<Models.User>> GetUserByDocumentId(string documentId)
+        {
+            try
+            {
+                Models.User user = await _client.ReadDocumentAsync<Models.User>(UriFactory.CreateDocumentUri(
+                    _dbName, _collectionName, documentId));
+                return Ok(user);
+            }
+            catch (DocumentClientException de)
+            {
+                switch (de.StatusCode.Value)
+                {
+                    case System.Net.HttpStatusCode.NotFound:
+                        return NotFound();
+                }
+            }
+            return BadRequest();
+        }
+
     }
 }
--- kimppakyyti-api/KimppakyytiApi/KimppakyytiApi/Models/Ride.cs
+++ kimppakyyti-api/KimppakyytiApi/KimppakyytiApi/Models/Ride.cs
@@ -15,11 +15,21 @@ namespace KimppakyytiApi.Models
 
         public string City { get; set; }
         public string Address { get; set; }
-        public Dictionary<DateTime, bool> Frequency { get; set; }
-
+        //public Dictionary<DateTime, bool> Frequency { get; set; }
+        public bool MondayFrequency { get; set; }
+        public bool TuesayFrequency { get; set; }
+        public bool WednesdayFrequency { get; set; }
+        public bool ThursdayFrequency { get; set; }
+        public bool FridayFrequency { get; set; }
+        public bool SaturdayFrequency { get; set; }
+        public bool SundayFrequency { get; set; }
+                
+       
         public override string ToString()
         {
-            return $"Nickname: {Nickname} Price: {Price} When: {When} City: {City} Address: {Address}";
+            return $"Nickname: {Nickname} Price: {Price} When: {When} City: {City} Address: {Address} Frequency: " +
+                $"Monday: {MondayFrequency} Tuesday: {TuesayFrequency} Wednesday: {WednesdayFrequency} Thursday: {ThursdayFrequency}" +
+                $"Friday: {FridayFrequency} Saturday: {SaturdayFrequency} Sunday: {SundayFrequency}";
         }
         public string ToJson()
         {
